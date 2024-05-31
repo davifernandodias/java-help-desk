10847c84dff35d5f289695ba46c89054602d1748
Perkles <davifernandosousadias18@gmail.com>
2024-05-31 19:16:34 -0300

    arquetura projeto

---
+++ src/main/java/com/systemupdate/beta/BetaApplication.java
@@ -0,0 +1,13 @@
+package com.systemupdate.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemupdate/beta/BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemupdate.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
