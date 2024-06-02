3a49f60241f12453f58e070d8b232826918c1e2f
Perkles <davifernandosousadias18@gmail.com>
2024-06-01 21:08:44 -0300

    alter controller login in home for controllerLogin

--- src/main/java/com/systemupdate/beta/controllers/HomeController.java
+++ src/main/java/com/systemupdate/beta/controllers/HomeController.java
@@ -49,18 +49,4 @@ public class HomeController {
         return "principal";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/login-error")
-    public String loginError(ModelMap model) {
-        model.addAttribute("alerta", "erro");
-        model.addAttribute("titulo", "Credenciais inválidas");
-        model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
-        model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativos.");
-
-        return "login";
-    }
 }
---
+++ src/main/java/com/systemupdate/beta/controllers/LoginController.java
@@ -0,0 +1,25 @@
+package com.systemupdate.beta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(ModelMap model) {
+        model.addAttribute("alerta", "erro");
+        model.addAttribute("titulo", "Credenciais inválidas");
+        model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+        model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativos.");
+
+        return "login";
+    }
+
+}
