b6b18481d7702743616349d96b5e63020fdbea65
Perkles <davifernandosousadias18@gmail.com>
2024-06-23 09:30:48 -0300

    🔨 implementando interface cadastrar colaborador

--- src/main/java/com/systemupdate/beta/controllers/colaboradores/CreateController.java
+++ src/main/java/com/systemupdate/beta/controllers/colaboradores/CreateController.java
@@ -1,11 +1,24 @@
 package com.systemupdate.beta.controllers.colaboradores;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.systemupdate.beta.models.Colaborador;
 
 public class CreateController {
     
-    @GetMapping("/castro")
-    public String HomeColaboradores(){
-        return "olá";
+    @GetMapping("/colaboradorCreate")
+    public String principal(ModelMap model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = auth.getName();
+        boolean isAuthenticated = auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken);
+
+        model.addAttribute("isAuthenticated", isAuthenticated);
+        model.addAttribute("userEmail", userEmail);
+        model.addAttribute("colaborador", new Colaborador());
+        return "cadastroColaborador";
     }
 }
