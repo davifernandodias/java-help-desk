4dd449f46a9e07d6ad356bc14125e92aa3a4d86d
Perkles <davifernandosousadias18@gmail.com>
2024-06-23 20:08:05 -0300

    🔨 implementando tabelas para informações adicionais do colaborador

---
+++ src/main/java/com/systemupdate/beta/models/Endereco.java
@@ -0,0 +1,97 @@
+package com.systemupdate.beta.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "endereco")
+public class Endereco extends AbstractEntity {
+
+    @Column(name = "logadouro")
+    private String logradouro;
+    @Column(name = "cidade")
+    private String cidade;
+    @Column(name = "estado")
+    private String estado;
+    @Column(name = "barrio")
+    private String bairro;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private String numero;
+    @Column(name = "cep")
+    private String cep;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "id_endereco", nullable = false)
+    private InformacaoAdicionaisColaborador informacaoAdicionaisColaborador;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public InformacaoAdicionaisColaborador getInformacaoAdicionaisColaborador() {
+        return informacaoAdicionaisColaborador;
+    }
+
+    public void setInformacaoAdicionaisColaborador(InformacaoAdicionaisColaborador informacaoAdicionaisColaborador) {
+        this.informacaoAdicionaisColaborador = informacaoAdicionaisColaborador;
+    }
+
+}
---
+++ src/main/java/com/systemupdate/beta/models/InformacaoAdicionaisColaborador.java
@@ -0,0 +1,44 @@
+package com.systemupdate.beta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "informacao_adicionais")
+public class InformacaoAdicionaisColaborador extends AbstractEntity {
+    @Column(name = "telefone")
+    private String telefone;
+    
+    @Column(name = "cpf")
+    private Integer cpf;
+    
+    @Column(name = "rg")
+    private String rg;
+
+    @OneToOne
+    @JoinColumn(name = "id_colaborador", nullable = false)
+    private Colaborador colaborador;
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+    public String getRg() {
+        return rg;
+    }
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
