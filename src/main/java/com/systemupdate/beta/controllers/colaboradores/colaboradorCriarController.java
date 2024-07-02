b07d7eb8e40a30e831548a234289cdd616e00947
Perkles <davifernandosousadias18@gmail.com>
2024-07-01 21:53:47 -0300

    ✅ implementado controller post cadastro de colaborador ALL info

--- src/main/java/com/systemupdate/beta/controllers/colaboradores/colaboradorCriarController.java
+++ src/main/java/com/systemupdate/beta/controllers/colaboradores/colaboradorCriarController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.systemupdate.beta.models.Colaborador;
+import com.systemupdate.beta.models.Endereco;
+import com.systemupdate.beta.models.InformacaoAdicionaisColaborador;
 import com.systemupdate.beta.models.Perfil;
 import com.systemupdate.beta.models.PerfilTipo;
 import com.systemupdate.beta.models.Usuario;
@@ -59,54 +61,101 @@ public class colaboradorCriarController {
     }
 
     @PostMapping("/criarColaboradores/salvar")
-    public String salvarUsuario(@ModelAttribute Usuario usuario,
-                                @RequestParam String email,
-                                @RequestParam String senha,
-                                @RequestParam PerfilTipo perfilTipo,
-                                @RequestParam String regiao,
-                                @RequestParam String nome,
-                                @RequestParam String cargo,
-                                @RequestParam LocalDate dataNascimento,
-                                @RequestParam Double salario,
-                                Model model) {
-    
-        // Busca o perfil com base no tipo escolhido
-        Perfil perfil = perfilRepository.findByDescricao(perfilTipo.getDescricao());
-    
-        // Verifica se o perfil foi encontrado
-        if (perfil != null) {
-            // Adiciona o perfil ao usuário
-            usuario.addPerfil(perfil);
-    
-            // Define outras propriedades do usuário
-            usuario.setAtivo(true);
-            usuario.setEmail(email);
-            usuario.setSenha(senha);
-    
-            // Verifica se o colaborador já existe
-            Colaborador colaborador = usuario.getColaborador();
-            if (colaborador == null) {
-                colaborador = new Colaborador();
-                colaborador.setUsuario(usuario); // Associa o colaborador ao usuário
-                usuario.setColaborador(colaborador); // Associa o colaborador ao usuário
-            }
-    
-            // Define as propriedades do colaborador
-            colaborador.setNome(nome);
-            colaborador.setCargo(cargo);
-            colaborador.setDataNascimento(dataNascimento);
-            colaborador.setSalario(salario);
-            colaborador.setRegiao(regiao);
-    
-            // Salva o usuário e o colaborador no banco de dados
-            usuarioRepository.save(usuario);
-    
-            // Redireciona para página adequada após salvar
-            return "redirect:/criarColaboradores";
-        } else {
-            throw new IllegalArgumentException("Perfil não encontrado: " + perfilTipo.getDescricao());
+public String salvarUsuario(@ModelAttribute Usuario usuario,
+                            @RequestParam String email,
+                            @RequestParam String senha,
+                            @RequestParam PerfilTipo perfilTipo,
+                            @RequestParam String regiao,
+                            @RequestParam String nome,
+                            @RequestParam String cargo,
+                            @RequestParam LocalDate dataNascimento,
+                            @RequestParam Double salario,
+                            @RequestParam String telefone,
+                            @RequestParam Integer cpf,
+                            @RequestParam String rg,
+                            @RequestParam String sobrenome,
+                            @RequestParam String logradouro,
+                            @RequestParam String cidade,
+                            @RequestParam String estado,
+                            @RequestParam String bairro,
+                            @RequestParam String rua,
+                            @RequestParam String numero,
+                            @RequestParam String cep,
+                            Model model) {
+
+    // Busca o perfil com base no tipo escolhido
+    Perfil perfil = perfilRepository.findByDescricao(perfilTipo.getDescricao());
+
+    // Verifica se o perfil foi encontrado
+    if (perfil != null) {
+        // Adiciona o perfil ao usuário
+        usuario.addPerfil(perfil);
+
+        // Define outras propriedades do usuário
+        usuario.setAtivo(true);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+
+        // Verifica se o colaborador já existe
+        Colaborador colaborador = usuario.getColaborador();
+        if (colaborador == null) {
+            colaborador = new Colaborador();
+            colaborador.setUsuario(usuario); // Associa o colaborador ao usuário
+            usuario.setColaborador(colaborador); // Associa o colaborador ao usuário
         }
+
+        // Define as propriedades do colaborador
+        colaborador.setNome(nome);
+        colaborador.setCargo(cargo);
+        colaborador.setDataNascimento(dataNascimento);
+        colaborador.setSalario(salario);
+        colaborador.setRegiao(regiao);
+
+        // Verifica se InformacaoAdicionaisColaborador já existe
+        InformacaoAdicionaisColaborador infoadd = colaborador.getInformacaoAdicionais();
+        if(infoadd == null){
+            infoadd = new InformacaoAdicionaisColaborador();
+            colaborador.setInformacaoAdicionais(infoadd);
+            infoadd.setColaborador(colaborador); // Associa a informação adicional ao colaborador
+        }
+
+        // Define as propriedades de InformacaoAdicionaisColaborador
+        infoadd.setCpf(cpf);
+        infoadd.setRg(rg);
+        infoadd.setTelefone(telefone);
+        infoadd.setSobrenome(sobrenome);
+
+        // Verifica se Endereco já existe
+        Endereco endereco = infoadd.getEndereco();
+        if(endereco == null){
+            endereco = new Endereco();
+            infoadd.setEndereco(endereco);
+            endereco.setInformacaoAdicionaisColaborador(infoadd); // Associa o endereço à informação adicional
+        }
+
+        // Define as propriedades do Endereco
+        endereco.setLogradouro(logradouro);
+        endereco.setCidade(cidade);
+        endereco.setEstado(estado);
+        endereco.setBairro(bairro);
+        endereco.setRua(rua);
+        endereco.setNumero(numero);
+        endereco.setCep(cep);
+
+        // Salva o usuário e o colaborador no banco de dados
+        usuarioRepository.save(usuario);
+
+        // Salva as informações adicionais e endereço no banco de dados
+        informacaoAddRepository.save(infoadd);
+        enderecoRepository.save(endereco);
+
+        // Redireciona para página adequada após salvar
+        return "redirect:/criarColaboradores";
+    } else {
+        throw new IllegalArgumentException("Perfil não encontrado: " + perfilTipo.getDescricao());
     }
+}
+
     
 
 }
--- src/main/java/com/systemupdate/beta/models/InformacaoAdicionaisColaborador.java
+++ src/main/java/com/systemupdate/beta/models/InformacaoAdicionaisColaborador.java
@@ -1,22 +1,16 @@
 package com.systemupdate.beta.models;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "informacao_adicionais")
 public class InformacaoAdicionaisColaborador extends AbstractEntity {
     @Column(name = "telefone")
     private String telefone;
-    
-    
+
     @Column(name = "cpf")
     private Integer cpf;
-    
+
     @Column(name = "rg")
     private String rg;
 
@@ -30,23 +24,27 @@ public class InformacaoAdicionaisColaborador extends AbstractEntity {
     @OneToOne(mappedBy = "informacaoAdicionaisColaborador", cascade = CascadeType.ALL)
     private Endereco endereco;
 
-
+    // Getters and Setters
     public String getSobrenome() {
         return sobrenome;
     }
+
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
+
     public Endereco getEndereco() {
         return endereco;
     }
+
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
-    
+
     public String getTelefone() {
         return telefone;
     }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
@@ -54,13 +52,24 @@ public class InformacaoAdicionaisColaborador extends AbstractEntity {
     public Integer getCpf() {
         return cpf;
     }
+
     public void setCpf(Integer cpf) {
         this.cpf = cpf;
     }
+
     public String getRg() {
         return rg;
     }
+
     public void setRg(String rg) {
         this.rg = rg;
+    }
+
+    public Colaborador getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(Colaborador colaborador) {
+        this.colaborador = colaborador;
     }
 }
